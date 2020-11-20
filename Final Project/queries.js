502f4e977a6344a4e348f4b935866060d6a48d35
Mongeese1 <60645887+Mongeese1@users.noreply.github.com>
2020-11-20 12:16:34 +0100

    fixed search queries to get only paths

--- Final Project/queries.js
+++ Final Project/queries.js
@@ -7,9 +7,9 @@ function GetQueryData(wbk_bio) {
     var responseJ1;
     var responseJ2;
     var responseJ3;
-    var query_wiki = "SELECT ?drug ?drugLabel ?keggid WHERE{ ?drug wdt:P2175 wd:Q81938  . ?drug wdt:P665 ?keggid . SERVICE wikibase:label {bd:serviceParam wikibase:language '[AUTO_LANGUAGE]' . }} LIMIT 10";
+    var query_wiki = "SELECT ?drug ?drugLabel ?keggid WHERE{ ?drug wdt:P2175 wd:Q81938  . ?drug wdt:P665 ?keggid . SERVICE wikibase:label {bd:serviceParam wikibase:language '[AUTO_LANGUAGE]' . }} LIMIT 100";
     
-    var query_drug = "PREFIX kegg_vocabulary: <http://bio2rdf.org/kegg_vocabulary:> SELECT ?pathway ?drug ?keggid WHERE { VALUES ?keggid {<http://bio2rdf.org/kegg:D07286>} ?keggid kegg_vocabulary:pathway ?pathway . ?drug kegg_vocabulary:pathway ?pathway }LIMIT 10";
+    var query_drug = "PREFIX kegg_vocabulary: <http://bio2rdf.org/kegg_vocabulary:> SELECT ?pathway ?drug ?keggid WHERE { VALUES ?keggid {<http://bio2rdf.org/kegg:D07286>} ?keggid kegg_vocabulary:pathway ?pathway }";
         
     fetch(wbk_bio.sparqlQuery(query_drug)
          ).then( response => response.json()
@@ -19,10 +19,10 @@ function GetQueryData(wbk_bio) {
         document.getElementById("output3").innerHTML = responseJ3;
         var response_drug = response;
         var pathways_drug = "";
-        for (i =0 ; i<response_drug.length;i++) {
-            pathways_drug += response_drug[i].pathway;
+     
+        pathways_drug = response_drug[0].pathway;
             //code
-        }
+        
         //console.log(pathways);
         fetch(wdk.sparqlQuery(query_wiki)
              ).then( response => response.json()
@@ -41,7 +41,7 @@ function GetQueryData(wbk_bio) {
             }
     //      console.log(keggValues);
     
-            var query_bio = "PREFIX kegg_vocabulary: <http://bio2rdf.org/kegg_vocabulary:> SELECT ?pathway ?drug ?keggid WHERE { VALUES ?keggid {"+keggValues+"} ?keggid kegg_vocabulary:pathway ?pathway . ?drug kegg_vocabulary:pathway ?pathway }LIMIT 10";
+            var query_bio = "PREFIX kegg_vocabulary: <http://bio2rdf.org/kegg_vocabulary:> SELECT ?pathway ?keggid WHERE { VALUES ?keggid {"+keggValues+"} ?keggid kegg_vocabulary:pathway ?pathway }";
     
             fetch(wbk_bio.sparqlQuery(query_bio)
              ).then( response => response.json()
@@ -50,20 +50,27 @@ function GetQueryData(wbk_bio) {
             responseJ2 = JSON.stringify(response, undefined, 2);
             document.getElementById("output2").innerHTML = responseJ2;
             var response_bio = response;
-            var pathways_bio= "";
+            var pathways_bio= [];
             for (i =0 ; i<response_bio.length;i++) {
-                pathways_bio+= response_bio[i].pathway;
+                pathways_bio[i]= response_bio[i].pathway;
                 //code
             }
+            var interact = [];
+            var nointeract =[];
             for (i=0;i<pathways_bio.length;i++) {
-                if (pathways_bio[i] == pathways_drug[i]) {
-                    console.log('1');
+                
+                if (pathways_bio[i] == pathways_drug) {
+                    interact = pathways_bio[i];
                     //code
                 }
+                else{
+                    nointeract[i] = pathways_bio[i];
+                }
+                
                 //code
             }
                 //code
-            
+            console.log(interact);
        
             });
         }); 
